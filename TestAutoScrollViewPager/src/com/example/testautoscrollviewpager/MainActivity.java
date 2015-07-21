9a6d5dccab89ab572c9ae8fa92bafe4888f81572
fattoliu <724684054@qq.com>
2015-07-21 15:13:07 +0800

    Signed-off-by: fattoliu <724684054@qq.com>

--- TestAutoScrollViewPager/src/com/example/testautoscrollviewpager/MainActivity.java
+++ TestAutoScrollViewPager/src/com/example/testautoscrollviewpager/MainActivity.java
@@ -25,41 +25,40 @@ public class MainActivity extends Activity {
 	private String[] text = { "摄影", "壁纸", "明星", "宠物", "动漫", "头像" };
 	ArrayList<String> urls = new ArrayList<>();
 	SplashShowAdapter adapter;
-	AutoScrollLoopViewPager viewpager ;
+	AutoScrollLoopViewPager viewpager;
 	CirclePageIndicator cpi;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		 viewpager = (AutoScrollLoopViewPager) findViewById(R.id.view_pager);
-		 cpi = (CirclePageIndicator) findViewById(R.id.cpi_indicator);
+		viewpager = (AutoScrollLoopViewPager) findViewById(R.id.view_pager);
+		cpi = (CirclePageIndicator) findViewById(R.id.cpi_indicator);
 		textView = (TextView) findViewById(R.id.tv_title);
 		adapter = new SplashShowAdapter(this, urls);
 		Button btn = (Button) findViewById(R.id.button1);
 		btn.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				urls.clear();
 				for (int i = 0; i < 6; i++) {
 					urls.add(args[i]);
 				}
-				
+
 				adapter.notifyDataSetChanged();
 				viewpager.setAdapter(adapter);
 				viewpager.startAutoScroll();
 				cpi.setViewPager(viewpager);
-				
-				
+
 			}
 		});
-
 
 		viewpager.setOnUpdateTitleListener(new OnUpdateTitleListener() {
 
 			@Override
 			public void updateTitle(int position) {
-					textView.setText(text[position]);					
+				textView.setText(text[position]);
 			}
 		});
 
